8c629e612dbec18891d1da4450a0d743c28a8ff0
snippetarnab <arnabvivo2020@gmail.com>
2024-07-02 12:13:22 +0530

    js file

---
+++ one.js
@@ -0,0 +1,11 @@
+const nameId = "09lop"
+var accountId ="Asmon"
+let password ="9090"
+namme = "Arnab"
+
+//  Do not use Var keyword
+
+password ="4567"
+accountId = "lipop"
+
+console.table([nameId, accountId, password, namme])
